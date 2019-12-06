5eee2e4fef7b96ecaa09576007f6477961f40066
Laura Xu <lauraxu13@gmail.com>
2019-12-06 17:06:49 -0500

    figured out base code

--- src/app/id4-bathroom/id4-bathroom.component.ts
+++ src/app/id4-bathroom/id4-bathroom.component.ts
@@ -1,35 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MasterService } from '../master.service';
+import { TotalScore } from '../total-score';
 
 @Component({
-  selector: 'app-id4-bathroom',
-  templateUrl: './id4-bathroom.component.html',
-  styleUrls: ['./id4-bathroom.component.css']
+  selector: "app-id4-bathroom",
+  templateUrl: "./id4-bathroom.component.html",
+  styleUrls: ["./id4-bathroom.component.css"]
 })
 export class Id4BathroomComponent implements OnInit {
-
-
-
   nextCounter: number = 0;
 
   hideDialogue: boolean = false;
-  showOpt:boolean=false
+  showOpt: boolean = false;
+  popularity:number;
+  charisma:number;
+  total: TotalScore;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private service: MasterService) {}
 
   nextButton() {
     this.nextCounter++;
-    if (this.nextCounter <= 1)  {
+    if (this.nextCounter <= 1) {
       this.hideDialogue = true;
+    } else {
+      this.showOpt = true;
     }
-    else {
-      this.showOpt =true
-    }
-     }
+  }
+
+  opt1id4() {
+    this.popularity = this.popularity +1
+    this.service.setTSpopularity(this.popularity)
+
+    this.charisma =this.charisma +1
+    this.service.setTScharisma(this.charisma)
+
+    this.router.navigate(["homework"]);
+
+    console.log(this.total)
+  }
+  opt2id4() {
+    this.popularity = this.popularity -1
+    this.service.setTSpopularity(this.popularity)
+
+    this.charisma =this.charisma -2
+    this.service.setTScharisma(this.charisma)
+    
+    this.router.navigate(["homework"]);
+    console.log(this.popularity)
+  }
 
 
-
-
-  ngOnInit() {}
+  ngOnInit() {
+this.popularity=this.service.getTSpopularity();
+this.charisma=this.service.getTScharisma();
+this.total=this.service.getTS();
+ 
+  }
 }
-
--- src/app/master.service.ts
+++ src/app/master.service.ts
@@ -16,18 +16,51 @@ export class MasterService {
   };
 
 
-  constructor(private http: HttpClient) {}
-
+ 
+constructor(private http: HttpClient) {}
 // set name from home page form to service
+setTSname(newInfo:string){
+  this.totalScore.name =newInfo;
+}
 
 // get Name from service to pages
+getTSname():string{
+  return this.totalScore.name;
+}
 
+// get score from service to  components
+getTSpopularity():number{
+  return this.totalScore.popularity;
+}
+
+getTScharisma():number{
+  return this.totalScore.charisma;
+}
+
+getTSacademia():number{
+  return this.totalScore.academia;
+}
 // set score from options to service array
+setTSpopularity(newInfo:number){
+  this.totalScore.popularity =newInfo;
+}
+
+setTScharisma(newInfo:number){
+  this.totalScore.charisma =newInfo;
+}
+
+setTSacademia(newInfo:number){
+  this.totalScore.academia =newInfo;
+}
 
 // get score from serice to Score page
+getTS():TotalScore{
+  return this.totalScore;
+}
+
 
 // Get from data base
 
 // Push into data base
-
+ 
 }
