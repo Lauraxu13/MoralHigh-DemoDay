74e4f30eeab86fc29e516449d71550bddc64c2a4
Laura Xu <lauraxu13@gmail.com>
2019-12-17 09:12:29 -0500

    adding music working on it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ɵflushModuleScopingQueueAsMuchAsPossible,
+  OnInit
+} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'MoralHigh-DemoDay';
+export class AppComponent implements OnInit{
+ 
+  title = "MoralHigh-DemoDay";
+
+  off: boolean = false;
+  backMusic = new Audio("assets/flatZone.mp3") ;
+
+
+  toggleSound() {
+    if (this.backMusic.paused) {
+    this.backMusic.play();
+    this.off = !this.off;
+  }
+    else this.backMusic.pause();
+    // !this.off = this.off;
+  } 
+  
+  ngOnInit() {
+    this.backMusic.play();
+
+
+  }
 }
